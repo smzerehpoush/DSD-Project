838c08c99eda7c825812adb0987111603cb9bcb0
Seyyed Mahdiyar Zerehpoush <mahdiyar.zerehpoush@gmail.com>
2021-07-28 15:48:15 +0430

    change print

--- src/main/java/me/mahdiyar/d3/Mos2Frame.java
+++ src/main/java/me/mahdiyar/d3/Mos2Frame.java
@@ -1,68 +1,65 @@
 package me.mahdiyar.d3;
 
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
+import java.awt.print.PageFormat;
 import java.awt.print.Printable;
 import java.awt.print.PrinterException;
 import java.awt.print.PrinterJob;
 
-public class Mos2Frame extends Frame {
+public class Mos2Frame extends Frame implements Printable, ActionListener {
 
-    private Button printButton;
-    private Mos2Panel mos2Panel;
 
     public Mos2Frame() throws HeadlessException {
         setLayout(new BorderLayout());
         setBackground(Color.lightGray);
-        printButton = new Button("print me!");
-        mos2Panel = new Mos2Panel();
+        Mos2Panel mos2Panel = new Mos2Panel();
         mos2Panel.setSize(800, 600);
         add("Center", mos2Panel);
-        add("North", printButton);
         setSize(800, 600);
         setVisible(true);
         addWindowListener(new WindowAdapter() {
-            @Override
             public void windowClosing(WindowEvent e) {
                 System.exit(0);
             }
         });
-    }
-
-    @Override
-    public boolean action(Event event, Object what) {
-        if (event.target == printButton) {
-            printComponent(mos2Panel);
-            return true;
-        }
-        return false;
-    }
-
-    public void printComponent(Component component) {
-
-        PrinterJob pj = PrinterJob.getPrinterJob();
-        pj.setJobName(" Print Component ");
-
-        pj.setPrintable((pg, pf, pageNum) -> {
-            if (pageNum > 0) return Printable.NO_SUCH_PAGE;
-
-            Graphics2D g2 = (Graphics2D) pg;
-            g2.translate(pf.getImageableX(), pf.getImageableY());
-            component.paint(g2);
-            return Printable.PAGE_EXISTS;
-        });
-
-        if (!pj.printDialog()) return;
-
-        try {
-            pj.print();
-        } catch (PrinterException ex) {
-            System.err.println(ex);
-        }
+        Button printButton = new Button("Print This Window");
+        printButton.addActionListener(this);
+        add("South", printButton);
     }
 
     public static void main(String[] args) {
-        new Mos2Frame();
+        Frame frame = new Mos2Frame();
+        frame.pack();
+        frame.setVisible(true);
     }
+
+    @Override
+    public int print(Graphics g, PageFormat pf, int page) {
+        if (page > 0) {
+            return NO_SUCH_PAGE;
+        }
+        Graphics2D g2d = (Graphics2D) g;
+        g2d.translate(pf.getImageableX(), pf.getImageableY());
+        this.printAll(g);
+        return PAGE_EXISTS;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        PrinterJob job = PrinterJob.getPrinterJob();
+        job.setPrintable(this);
+        boolean ok = job.printDialog();
+        if (ok) {
+            try {
+                job.print();
+            } catch (PrinterException ex) {
+                /* The job did not successfully complete */
+            }
+        }
+    }
+
 }
